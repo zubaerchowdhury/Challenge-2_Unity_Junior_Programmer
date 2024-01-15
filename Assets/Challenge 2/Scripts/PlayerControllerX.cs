cd62b18efe885a8abaeffb3e5e6d261dceeb375b
Zubaer Rahman Chowdhury <zrchy16@gmail.com>
2024-01-14 23:07:12 -0500

    First commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float spawnRate = 1.0f;
+    private float lastSpawn = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && ((Time.time - lastSpawn) > spawnRate))
+        {
+            lastSpawn = Time.time;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
